e46ce8fbeba54c09f7be7aa8a7c5d4f197c1cb74
IfeAwoyelu <awoyeluifeoluwa@gmail.com>
2023-07-27 07:01:30 +0100

    Added a few

---
+++ import numpy as np.py
@@ -0,0 +1,4 @@
+import numpy as np 
+import pandas as pd
+print('Hello World')
+ Name = pd.read_csv('/Users/mac/Documents/Data Science/Data Set/Feature Engineering/1669096535_feature_engineering_dataset/PEP1.csv')
